b7a3e5c9c96622938317ceefd78be32e6b7aadaf
Huynh Duc Tam <huynhductam69@gmail.com>
2024-09-11 12:24:21 +0700

    first commit

---
+++ src/test/java/dev/ducku/samplejenkins/SampleJenkinsApplicationTests.java
@@ -0,0 +1,14 @@
+package dev.ducku.samplejenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleJenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("HELLO WORLD");
+    }
+
+}
